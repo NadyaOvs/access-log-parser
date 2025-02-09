0c9c5558b1179a048dba154de13cf3d69696cf81
Nadezhda Ovsyannikova <ovsyannikovanr@gmail.com>
2025-02-09 13:16:37 +0400

    mathematical operations with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = (number1 + number2);
+        int diff = (number1 - number2);
+        int mult = (number1 * number2);
+        double div = (double) number1 / number2;
+        System.out.println("Сумма: "+ sum);
+        System.out.println("Разность: "+ diff);
+        System.out.println("Произведение: "+ mult);
+        System.out.println("Частное: "+ div);
     }
 }
