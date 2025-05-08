81a44d6c7b819d071d108cf5a90a2bfdf0bab03d
Nadezhda Ovsyannikova <ovsyannikovanr@gmail.com>
2025-05-08 14:44:39 +0400

    task2: comparing objects

--- src/Path.java
+++ src/Path.java
@@ -5,53 +5,58 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Path {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int count = 0;
-        boolean fileExists;
-        boolean isDirectory;
-        System.out.println("Укажите путь в файлу: ");
+        System.out.println("Укажите путь к файлу: ");
+
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
-            fileExists = file.exists();
-            isDirectory = file.isDirectory();
-            if (!fileExists || isDirectory) {
+            if (!file.exists() || file.isDirectory()) {
                 System.out.println("Файл не существует или путь ведет к папке, а не к файлу");
                 continue;
             }
-            if (fileExists) {
-                System.out.println("Путь указан верно");
-                count++;
-                System.out.println("Это файл номер " + count);
-            }
+            System.out.println("Путь указан верно");
+            count++;
+            System.out.println("Это файл номер " + count);
+
+            int googlebotCount = 0;
+            int yandexbotCount = 0;
 
             int lineCount = 0;
-            int maxLength = 0;
-            int minLength = Integer.MAX_VALUE;
-
-            try {
-                FileReader fileReader = new FileReader(path);
-                BufferedReader reader = new BufferedReader(fileReader);
+            try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
                 String line;
 
                 while ((line = reader.readLine()) != null) {
-                    int length = line.length();
-
-                    if (length > 1024) {
-                        throw new LineTooLongException("Ошибка: строка длиной " + length + " символов превышает максимально допустимую длину 1024 символа.");
+                    if (line.length() > 1024) {
+                        throw new LineTooLongException("Ошибка: строка длиной " + line.length() + " символов превышает максимально допустимую длину 1024 символа.");
                     }
 
                     lineCount++;
 
-                    if (length > maxLength) {
-                        maxLength = length;
-                    }
-                    if (length < minLength) {
-                        minLength = length;
+                    String userAgent = line;
+
+                    int startIndex = userAgent.indexOf("(");
+                    int endIndex = userAgent.indexOf(")");
+
+                    if (startIndex != -1 && endIndex != -1 && startIndex < endIndex) {
+                        String firstBrackets = userAgent.substring(startIndex + 1, endIndex);
+                        String[] parts = firstBrackets.split(";");
+                        if (parts.length >= 2) {
+                            String fragment = parts[1].trim();
+                            String program = fragment.split("/")[0].trim();
+
+
+                            if (program.equals("Googlebot")) {
+                                googlebotCount++;
+                            } else if (program.equals("YandexBot")) {
+                                yandexbotCount++;
+                            }
+                        }
                     }
                 }
 
-                reader.close();
+
             } catch (LineTooLongException ex) {
                 System.err.println(ex.getMessage());
                 return;
@@ -61,22 +66,25 @@ public class Path {
 
             System.out.println("Общее количество строк в файле: " + lineCount);
             if (lineCount > 0) {
-                System.out.println("Длина самой длинной строки: " + maxLength);
-                System.out.println("Длина самой короткой строки: " + minLength);
+
+                double googlebotShare = (double) googlebotCount / lineCount * 100;
+                double yandexbotShare = (double) yandexbotCount / lineCount * 100;
+                System.out.printf("Доля запросов от Googlebot: %.2f%%\n", googlebotShare);
+                System.out.printf("Доля запросов от YandexBot: %.2f%%\n", yandexbotShare);
             } else {
                 System.out.println("Файл пуст.");
             }
         }
     }
 
-
     static class LineTooLongException extends RuntimeException {
         public LineTooLongException(String message) {
             super(message);
         }
     }
+}
 
-    }
+
 
 //C:\Users\1\Desktop\StepUp\access.log
 //C:\Users\1\Desktop\StepUp\Курсовой проект.docx
